6371f6af6f08642ad4807035e233222da66f3296
nickell2008 <nickell2008@gmail.com>
2019-01-09 01:33:44 +0200

    Подправил посадку на 4м этаже

--- src/com/test/Lift2.java
+++ src/com/test/Lift2.java
@@ -5,9 +5,9 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Lift2 {
-    enum Status{UP, DOWN, STOP}
+    enum Status {UP, DOWN, STOP}
 
-    Integer currStage=1;
+    Integer currStage = 1;
     Status currStatus;
     Status prevStatus;
 
@@ -15,54 +15,55 @@ public class Lift2 {
     Set<Integer> goUp = new LinkedHashSet<>();
     Set<Integer> goDown = new LinkedHashSet<>();
 
-    public void go(){
-        if(currStage==1) currStatus = Status.UP;
-        if(currStage==4) currStatus = Status.DOWN;
+    public void go() {
+        if (currStage == 1) currStatus = Status.UP;
+        if (currStage == 4) currStatus = Status.DOWN;
 
-        while (currStatus == Status.UP&& currStage!=4){
+        while (currStatus == Status.UP && currStage != 4) {
 
             if (call.contains(currStage)) {
                 call.remove(currStage);
-                System.out.println("Этаж " + currStage+". Подбираем человека");
+                System.out.println("Этаж " + currStage + ". Подбираем человека");
+            }
+            currStage++;
+            if (goUp.contains(currStage)) {
+                goUp.remove(currStage);
+                System.out.println("Этаж " + currStage + ". Высаживаю человека");
             }
             if (goUp.isEmpty()) currStatus = Status.DOWN;
-            else System.out.println("Этаж "+ currStage+ ". Лифт едет вверх");
-            currStage++;
-            if (goUp.contains(currStage)){
-                goUp.remove(currStage);
-                System.out.println("Этаж " + currStage+". Высаживаю человека");
-            }
+            else System.out.println("Этаж " + currStage + ". Лифт едет вверх");
 
         }
 
-        while (currStatus == Status.DOWN&& currStage!=1){
-            System.out.println("Этаж "+ currStage+ ". Лифт едет вниз");
-            currStage--;
+        while (currStatus == Status.DOWN && currStage != 1) {
             if (call.contains(currStage)) {
                 call.remove(currStage);
-                System.out.println("Этаж " + currStage+". Подбираем человека");
+                System.out.println("Этаж " + currStage + ". Подбираем человека");
             }
             if (goDown.isEmpty()) currStatus = Status.UP;
-            if (goDown.contains(currStage)){
+            else System.out.println("Этаж " + currStage + ". Лифт едет вниз");
+            currStage--;
+            if (goDown.contains(currStage)) {
                 goDown.remove(currStage);
-                System.out.println("Этаж " + currStage+". Высаживаю человека");
+                System.out.println("Этаж " + currStage + ". Высаживаю человека");
             }
 
 
         }
     }
-    public void call(Integer from, Integer to){
+
+    public void call(Integer from, Integer to) {
         call.add(from);
-        if(!goUp.isEmpty() && goUp.stream().max(Comparator.comparing(Integer::valueOf)).get()>to)
+        if (!goUp.isEmpty() && goUp.stream().max(Comparator.comparing(Integer::valueOf)).get() > to)
             goDown.add(to);
         else goUp.add(to);
     }
 
-    public void stop(){
-        if(currStatus == Status.STOP){
+    public void stop() {
+        if (currStatus == Status.STOP) {
             System.out.println("Лифт поехал");
-            currStatus = prevStatus;}
-        else {
+            currStatus = prevStatus;
+        } else {
             System.out.println("Лифт остановился");
             prevStatus = currStatus;
             currStatus = Status.STOP;
